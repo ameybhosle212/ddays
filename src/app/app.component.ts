30ca3863a9ca86c0d33d12f5379dd8eac29e6da4
ameybhosle212 <ameybhosle212@gmail.com>
2022-07-06 17:45:37 +0530

    Done everything except select for all day button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,92 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'addstar';
+  getAllDayddat:any = {
+    'sunday':{
+      'checked':0,
+      'data':[]
+    },
+    'monday':{
+      'checked':0,
+      'data':[]
+    },
+    'tuesday':{
+      'checked':0,
+      'data':[]
+    },
+    'wednesday':{
+      'checked':0,
+      'data':[]
+    },
+    'thursday':{
+      'checked':0,
+      'data':[]
+    },
+    'friday':{
+      'checked':0,
+      'data':[]
+    },
+    'saturday':{
+      'checked':0,
+      'data':[]
+    }
+  }
+
+  updatedMonth(data:string){
+    console.log(this.getAllDayddat[data]);
+    
+    if(!this.getAllDayddat[data]['checked']){
+      this.getAllDayddat[data]['checked'] = 1 ;
+      this.getAllDayddat[data]['data'].push({
+        'inTime':'',
+        'outTime':''
+      })
+    }else{
+      this.getAllDayddat[data]['data'].length = 0
+      this.getAllDayddat[data]['checked'] = 0 ;
+    }
+    
+    console.log(this.getAllDayddat[data]);
+    
+  }
+  addTimeInSunday(){
+    console.log("jadbjda");
+    
+    this.getAllDayddat['sunday']['data'].push({
+      'inTime':'',
+      'outTime':''
+    })
+    console.log(this.getAllDayddat['sunday']['data']);
+    
+  }
+
+  addTimeInDays(day:any){
+    console.log("jadbjda");
+    
+    this.getAllDayddat[day]['data'].push({
+      'inTime':'',
+      'outTime':''
+    })
+    console.log("Day is ",day,"  ",this.getAllDayddat[day]['data']);
+    
+  }
+
+  updateInTimeChange(data:any, value1:any){
+    this.getAllDayddat['sunday']['data'][parseInt(data)]['inTime'] = value1.target.value;
+    console.log("INTIME ",this.getAllDayddat['sunday']['data']);
+    
+  }
+
+  updateOutTimeChange(data:any, value1:any){
+    this.getAllDayddat['sunday']['data'][parseInt(data)]['outTime'] = value1.target.value;
+    console.log("OUTTIME ",this.getAllDayddat['sunday']['data']);
+ }
+
+  deleteTimeFromdays(data:any,day:any){
+    if(parseInt(data)+1 == this.getAllDayddat[day]['data'].length && this.getAllDayddat[day]['data'].length != 1){
+      this.getAllDayddat[day]['data'].pop()
+    }
+    console.log(this.getAllDayddat);
+    
+  }
 }
